7f38954074d2fb58ffba60ba6376453f93fc680e
Jordan Kaye <jordan.kaye2@gmail.com>
2022-08-10 05:37:08 -0600

    Adds ast namespace and build files

---
+++ ast/node.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace jackal::ast
+{
+struct AbstractSyntaxNode
+{
+};
+}  // namespace jackal::ast
--- bytecode/opcode.hpp
+++ bytecode/opcode.hpp
@@ -5,8 +5,8 @@ namespace jackal::bytecode
 enum class opcode : unsigned char
 {
   constant = 0,  // r_dest, val
-  add = 1,       // r_dest, r_1, r_0
-  store = 2,     // l_dest, r_0
+  add = 1,       // r_dest, r_src0, r_src1
+  store = 2,     // l_dest, r_src
   load = 3,      // r_dest, l_src
   print = 4      // r_src
 };
