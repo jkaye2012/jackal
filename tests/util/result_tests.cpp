46d9793a61ce0e6acf931a80c907f60410511921
Jordan Kaye <jordan.kaye2@gmail.com>
2022-08-12 02:12:03 -0600

    Adds Result utility
    The Result struct models the possibility of failure for an
    operation. This struct is one of the few implementations that are
    currently expected to be used beyond the first phase Jackal
    implementation; as such, this class is fully documented and tested to
    production quality.

---
+++ tests/util/result_tests.cpp
@@ -0,0 +1,60 @@
+#include <functional>
+#include <string>
+
+#include "catch.hpp"
+#include "util/result.hpp"
+
+using Result = jackal::util::Result<int, std::string>;
+static std::function<int(int const&)> const same_type_mapper = [](int i) { return i * 2; };
+static std::function<double(int const&)> const different_type_mapper = [](int i)
+{ return 0.5 * i; };
+static std::function<std::string(std::string const&)> const error_mapper = [](std::string const& e)
+{ return e.substr(3); };
+
+TEST_CASE("Result constructed from ok value should be ok", "[result]")
+{
+  auto result = Result::from(12190);
+  REQUIRE(result.is_ok());
+}
+
+TEST_CASE("Result constructed from err value should be err", "[result]")
+{
+  auto result = Result::from("oh no");
+  REQUIRE(result.is_err());
+}
+
+TEST_CASE("Result should be able to return ok value", "[result]")
+{
+  auto result = Result::from(81590);
+  REQUIRE(result.ok() == 81590);
+}
+
+TEST_CASE("Result should be able to return err value", "[result]")
+{
+  auto result = Result::from("foobar");
+  REQUIRE(result.err() == "foobar");
+}
+
+TEST_CASE("Result::map with ok value should return a transformed Result", "[result]")
+{
+  auto result = Result::from(123);
+  REQUIRE(result.map(same_type_mapper).ok() == 246);
+}
+
+TEST_CASE("Result::map_err with err value should return a transformed Result", "[result]")
+{
+  auto result = Result::from("foobar");
+  REQUIRE(result.map_err(error_mapper).err() == "bar");
+}
+
+TEST_CASE("Result::map_flat with ok value should return a transformed Result", "[result]")
+{
+  auto result = Result::from(10);
+  REQUIRE(result.map_flat(different_type_mapper, error_mapper).ok() == Approx(5.0));
+}
+
+TEST_CASE("Result::map_flat with err value should return a transformed Result", "[result]")
+{
+  auto result = Result::from("baderror");
+  REQUIRE(result.map_flat(different_type_mapper, error_mapper).err() == "error");
+}
---
+++ util/result.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <functional>
+#include <utility>
+#include <variant>
+
+namespace jackal::util
+{
+/// @brief Models the possibility of failure for an operation.
+///
+/// Jackal's implementation uses Result over exceptions wherever possible.
+/// Exceptions are thrown only when they are not meant to be handled (e.g. when a bug or programmer
+/// error is encountered).
+///
+/// Result provides a basic monadic interface to simplify contextual error
+/// handling and to facilitate a more "functional" style of composition.
+///
+/// A successful Result is referred to as "ok", while a failed Result is referred to as an "err".
+///
+/// Does not support T and Err of the same type.
+///
+/// @tparam T the type contained by a successful operation
+/// @tparam Err the type contained by a failed operation
+template <typename T, typename Err>
+struct Result
+{
+  /// @brief Constructs an ok Result.
+  ///
+  /// @param result the value to wrap
+  static constexpr Result from(T result) noexcept { return Result(std::move(result)); }
+  /// @brief Constructs an err Result.
+  ///
+  /// @param error the error to wrap
+  static constexpr Result from(Err error) noexcept { return Result(std::move(error)); }
+
+  /// @brief Applies a function to the Result if it is ok, does nothing if it is err.
+  ///
+  /// @param func the function to apply to an ok Result
+  template <typename U>
+  [[nodiscard]] constexpr Result<U, Err> map(std::function<U(T const&)> const& func) const noexcept
+  {
+    if (is_ok())
+    {
+      return Result<U, Err>::from(func(ok()));
+    }
+    return Result<U, Err>::from(err());
+  }
+
+  /// @brief Applies a function to the Result if it is err, does nothing if it is ok.
+  ///
+  /// @param func the function to apply to an err Result
+  template <typename UErr>
+  [[nodiscard]] constexpr Result<T, UErr> map_err(
+      std::function<UErr(Err const&)> const& func) const noexcept
+  {
+    if (is_ok())
+    {
+      return Result<T, UErr>::from(ok());
+    }
+    return Result<T, UErr>::from(func(err()));
+  }
+
+  /// @brief Applies a function to the Result depending upon its state.
+  ///
+  /// @param okFunc the function to apply to an ok Result
+  /// @param errFunc the function to apply to an err Result
+  template <typename U, typename UErr>
+  [[nodiscard]] constexpr Result<U, UErr> map_flat(
+      std::function<U(T const&)> const& okFunc,
+      std::function<UErr(Err const&)> const& errFunc) const noexcept
+  {
+    if (is_ok())
+    {
+      return Result<U, UErr>::from(okFunc(ok()));
+    }
+    return Result<U, UErr>::from(errFunc(err()));
+  }
+
+  /// @returns whether the Result is in the ok state
+  [[nodiscard]] constexpr bool is_ok() const noexcept { return std::holds_alternative<T>(_result); }
+
+  /// @returns the value of an ok Result; terminates the program if the Result is err
+  [[nodiscard]] constexpr T ok() const noexcept { return std::get<T>(_result); }
+
+  /// @returns whether the Result in in the err state
+  [[nodiscard]] constexpr bool is_err() const noexcept
+  {
+    return std::holds_alternative<Err>(_result);
+  }
+
+  /// @returns the value of an err Result; terminates the program if the Result is ok
+  [[nodiscard]] constexpr Err err() const noexcept { return std::get<Err>(_result); }
+
+ private:
+  explicit constexpr Result(T result) noexcept : _result(std::move(result)) {}
+  explicit constexpr Result(Err error) noexcept : _result(std::move(error)) {}
+
+  std::variant<T, Err> _result;
+};
+}  // namespace jackal::util
