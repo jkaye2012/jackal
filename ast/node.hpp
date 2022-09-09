5af893867f4cdd81930cb8663598e947df1a49e9
Jordan Kaye <jordan.kaye2@gmail.com>
2022-09-09 13:38:29 -0600

    Removes temporary comment

--- ast/node.hpp
+++ ast/node.hpp
@@ -4,12 +4,6 @@
 namespace jackal::ast { struct Visitor; }
 // clang-format on
 
-// To continue: pimpl-ize all AST nodes to validate that this allows for compound
-// arithmetic expressions. Generally, this will add some overhead to the compilation
-// process (because pointers into each node will have to be chased), but it should also
-// allow general composition of nodes without worry of concrete memory format (e.g.
-// circumventing the current circular reference between Expresison and Operator).
-
 namespace jackal::ast
 {
 struct AbstractSyntaxNode
